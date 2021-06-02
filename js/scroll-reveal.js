685d7ee872efb8d266afbf48b7bbb00d308bd718
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-02 14:53:14 -0400

    Update

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -3,19 +3,19 @@
 // *******************************
 
 // -----  Main header -----
-ScrollReveal().reveal(".main-header__navbar", {
+ScrollReveal().reveal(".main-header-navbar", {
+  duration: 2000,
+  delay: 500,
+  origin: "top",
+  distance: "2rem"
+});
+ScrollReveal().reveal(".main-header-content-principal", {
   duration: 2000,
   delay: 500,
   origin: "left",
+  distance: "2rem"
 });
-ScrollReveal().reveal(".main-header__content", {
-  duration: 2000,
-  delay: 500,
-  origin: "left",
-  distance: "2rem",
-  visibility: "hidden"
-});
-ScrollReveal().reveal(".main-header__illustration", {
+ScrollReveal().reveal(".main-header-content-principal__illustration", {
   duration: 2000,
   delay: 500,
   origin: "right",
@@ -23,119 +23,46 @@ ScrollReveal().reveal(".main-header__illustration", {
 });
 
 // -----  Main content -----
-ScrollReveal().reveal(".main-content", {
+ScrollReveal().reveal(".stats-section__reference", {
   duration: 2000,
   delay: 500,
   origin: "bottom",
+  distance: "3rem",
 });
 
-// ----- About section -----
-ScrollReveal().reveal(".about-section", {
-  duration: 2000,
-  delay: 1000,
-  origin: "bottom",
-  distance: "10rem",
-});
-
-// ----- Features section -----
-ScrollReveal().reveal(".features-section__illustration", {
+ScrollReveal().reveal(".why-us-section__illustration", {
   duration: 2000,
   delay: 500,
   origin: "left",
-  distance: "2rem",
+  distance: "3rem",
 });
-ScrollReveal().reveal(".features-section__content-wrapper", {
+
+ScrollReveal().reveal(".why-us-section__content", {
   duration: 2000,
   delay: 500,
   origin: "right",
-  distance: "2rem",
+  distance: "3rem",
 });
 
-// ----- Pricing section -----
-ScrollReveal().reveal(".pricing-section__title", {
+ScrollReveal().reveal(".benefits-section__title", {
   duration: 2000,
   delay: 500,
   origin: "left",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".pricing-section__description", {
-  duration: 2000,
-  delay: 500,
-  origin: "right",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".pricing-section__info-card", {
-  duration: 2000,
-  delay: 1000,
-  origin: "bottom",
+  distance: "3rem",
 });
 
-// -----  Global network section -----
-ScrollReveal().reveal(".global-section__title", {
-  duration: 2000,
-  delay: 500,
-  origin: "left",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".global-section__description", {
+ScrollReveal().reveal(".benefits-section__description", {
   duration: 2000,
   delay: 500,
   origin: "right",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".global-section__illustration", {
-  duration: 2000,
-  delay: 1000,
-  origin: "bottom",
-  distance: "10rem",
+  distance: "3rem",
 });
 
-// -----  Sponsors section -----
-ScrollReveal().reveal(".sponsors-section__logo", {
+
+ScrollReveal().reveal(".card-info", {
   duration: 2000,
-  delay: 1000,
+  delay: 500,
   origin: "bottom",
-  distance: "10rem",
+  distance: "3rem",
 });
 
-// -----  Testimonials section -----
-ScrollReveal().reveal(".testimonials-section__title", {
-  duration: 2000,
-  delay: 750,
-  origin: "left",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".testimonials-section__description", {
-  duration: 2000,
-  delay: 750,
-  origin: "right",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".testimonials-section__testimonial", {
-  duration: 2000,
-  delay: 1200,
-  origin: "bottom",
-  distance: "10rem",
-});
-
-// -----  CTA aside section -----
-ScrollReveal().reveal(".cta-section", {
-  duration: 2000,
-  delay: 1000,
-  origin: "bottom",
-  distance: "10rem",
-});
-
-// -----  Main footer -----
-ScrollReveal().reveal(".main-footer", {
-  duration: 2000,
-  delay: 1000,
-  origin: "bottom",
-  distance: "10rem",
-});
-ScrollReveal().reveal(".attribution p", {
-  duration: 2000,
-  delay: 1000,
-  origin: "left",
-  distance: "2rem",
-});
